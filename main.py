175a9d5f9b05aabdbbb99b3f6e018fa9926462da
VHelbig <43109146+VHelbig@users.noreply.github.com>
2024-11-09 17:47:18 +0100

    Supper crappy kindof works barely

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#ro
 """
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor,ColorSensor, GyroSensor
+from pybricks.ev3devices import Motor,ColorSensor, GyroSensor, UltrasonicSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 from time import time
 from LineFollowBeh import LinefollowBeh,PIDParam,Devices
 from RampBeh import RampBeh,RampParam
+from gripper import operate_gripper
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
@@ -32,6 +33,7 @@ leftSensor=ColorSensor(Port.S1)
 rightSensor=ColorSensor(Port.S4)
 AngleSensor=GyroSensor(Port.S3)
 AngleSensor.reset_angle(0)
+UltraSensor=UltrasonicSensor(Port.S2)
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
@@ -53,12 +55,47 @@ LineFollower=LinefollowBeh(devices,pidParam,threshold,baseSpeed)
 RampAdjuster=RampBeh(AngleSensor,rampParams,baseSpeed)
 speed=baseSpeed
 
+prevdistance=0
+once=False
+def GrabCan():
+    robot.stop()
+    robot.turn(-10)
+    
+    global once
+    if once==True:
+        return
+    distance=UltraSensor.distance()
+    ev3.speaker.beep(500,200)
+    while(distance>40):
+        robot.drive(speed,0)
+        distance=UltraSensor.distance()
+    robot.stop()
+    robot.straight(20)
+    ev3.speaker.beep(500,100)
+    operate_gripper()
+    robot.turn(230)
+    robot.stop()
+    once=True
+
 while True:
+    distance=UltraSensor.distance()
+    d_diff=distance-prevdistance
+    prevdistance=distance
+    
+
+    #ev3.screen.print(distance)
+    if distance>90 and distance<200:
+        if d_diff<-10:
+            GrabCan()
+
     turning=LineFollower.GetAction()
     
-    speed=RampAdjuster.GetAction()
+    #speed=RampAdjuster.GetAction()
 
     robot.drive(speed,turning)
+
+
+
 
     
 
